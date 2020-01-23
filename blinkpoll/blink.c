e3ed2473900b6396f53c89550518df1598426ec5
Noah D'Souza <noah.dsouza@students.olin.edu>
2020-01-23 18:03:10 -0500

    Following this tutorial even though I know what I'm doing

--- blinkpoll/blink.c
+++ blinkpoll/blink.c
@@ -1,7 +1,7 @@
 /*
 ** Copyright (c) 2018, Bradley A. Minch
 ** All rights reserved.
-**
+** okay
 ** Redistribution and use in source and binary forms, with or without
 ** modification, are permitted provided that the following conditions are met: 
 ** 
